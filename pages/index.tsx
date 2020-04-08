3b206c391657b97766c19e750d87c3072b08a537
katsumi_takano <hunzyliam@gmail.com>
2020-04-08 23:58:37 +0900

    update: styled-components導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import { NextPage } from 'next';
 
-const Home: NextPage<{ userAgent: string }> = (props) => <h1>Hello world! - user agent: {props.userAgent}</h1>;
+const Home: NextPage<{ userAgent: string }> = (props) => (
+  <>
+    <Title>Hello world!</Title>
+    <UA>user agent: {props.userAgent}</UA>
+  </>
+);
 
 Home.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
   return { userAgent };
 };
+
+const Title = styled.h1`
+  font-size: 36px;
+`;
+
+const UA = styled.p`
+  color: red;
+  font-size: 24px;
+`;
 
 export default Home;
